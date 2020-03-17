278340006eb611021080fd01f53554f22da79a96
djoun52 <33383124+djoun52@users.noreply.github.com>
2020-03-17 11:34:33 +0100

    index

---
+++ exo/index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1>
+            exo Elan
+        </h1>
+    </header>
+
+
+    <main>
+        <div>
+            <a href=" ./exo1.php" title="exo1">exo1 fait</a>
+            <p>
+            Soit la phrase «Notre formation DL commence aujourd'hui».Ecrire  un  algorithme  permettant  de  compter  le  nombre  de  caractères  contenus  dans  cette  phrase (espaces inclus).
+            </p>
+        </div>
+        <hr>
+
+        <div>
+            <a href=" ./exo2.php" title="exo2">exo2 fait  </a>
+            <p>
+            A partir de la phrase de l’exercice 1, écrire l’instruction permettant de compter le nombre de mots contenus dans celle-ci.           
+          </p>
+        </div>
+        <hr>
+
+        <div>
+            <a href=" ./exo3.php" title="exo3">exo3 fait</a>
+            <p>
+            A partir de la phrase de l’exercice 1, écrire l’instruction permettant de remplacer le mot « aujourd’hui » par le mot « demain ». Afficher l’ancienne et la nouvelle phrase.      
+            </p>   
+        </div>
+        <hr>
+
+        <div>
+            <a href=" ./exo4.php" title="exo4">exo4 fait</a>
+            <p>
+            Ecrire un algorithme permettant de savoir si une phrase est palindrome.
+            </p>
+        </div>
+        <hr>
+        <div>
+            <a href=" ./exo5.php" title="exo5">exo5 fait</a>
+            <p>
+            Ecrire un algorithme qui déclare une valeur en francs et qui la convertit en euros.  Attention, la valeur générée devra être arrondie à 2 décimales.
+            </p>
+        </div>
+        <hr>
+        <div>
+            <a href=" ./exo6.php" title="exo6">exo6 fait</a>
+            <p>
+            Ecrire un algorithme permettant de calculer un montant de facture à régler à partir de la quantité d’articles, son prix hors taxe et un taux de TVA (exprimé en décimal. Ex : 20 % -> 0.2)
+            </p>
+        </div>
+        <hr>
+        <div>
+            <a href=" ./exo7.php" title="exo7">exo7 fait</a>
+            <p>
+            Ecrire un algorithme permettant de renvoyer la catégorie d’un enfant en fonction de son âge.
+            </p>
+        </div>
+        <hr>
+        <div>
+            <a href=" ./exo8.php" title="exo8">exo8 fait</a>
+            <p>
+            Ecrire un algorithme qui renvoie la table de multiplication d’un nombre passé en paramètre 
+            </p>
+        </div>
+        <hr>
+        <div>
+            <a href=" ./exo9.php" title="exo9">exo9 fait </a>
+            <p>
+            Nous souhaitons savoir si une personne est imposable en fonction de son âge et de son sexe. <br>
+            Si la personne est une femme dont l’âge est compris entre 18 et 35 ans ou si c’est un homme de plus de 20 ans, alors celle-ci est imposable (sinon ce n’est pas le cas, « non imposable »). 
+            </p>
+        </div>
+        <hr>
+        <div>
+            <a href=" ./exo10.php" title="exo10">exo10 boucle for</a> 
+            <br>
+            <a href=" ./exo10bis.php" title="exo10bis">exo10bis intdiv</a>
+            <p>
+            A partir d’un montant à payer et d’une somme versée pour régler un achat, écrire l’algorithme qui affiche à un utilisateur un rendu de monnaie en nombre de billets de 10 € et 5 €, pièces de 2 € et 1 €.
+            </p>
+        </div>
+        <hr>
+        <div>
+            <a href=" ./exo11.php" title="exo11">exo11 fait</a>
+            <p>
+            Initialiser un tableau de x marques de voitures. Ecrire un algorithme permettant de parcourir ce tableau et d’en afficher le contenu (une marque de voiture par ligne). Il est également demandé d’afficher le nombre de marques de voitures présentes dans le tableau. 
+            </p>
+        </div>
+        <hr>
+        <div>
+            <a href=" ./exo12.php" title="exo12">exo12 fait</a>
+            <p>
+            A partir d’une fonction personnalisée et à partir d’un tableau de prénoms et de langue associée (tableau contenant clé et valeur), dire bonjour aux différentes personnes dans leur langue respective (français  « Salut », anglais  « Hello », espagnol  « Hola ») 
+            </p>
+        </div>
+        <hr>
+        <div>
+            <a href=" ./exo13.php" title="exo13">exo13 fait</a>
+            <p>
+            Calculer la moyenne générale d'un élève dont les notes sont renseignées dans un tableau (pas de coefficient). Elle devra être affichée avec 2 décimales.      
+            </p>
+        </div>
+        <hr>
+        <div>
+            <a href=" ./exo14.php" title="exo14">exo14 fait</a>
+            <p>
+            Calculer l'âge exact d'une personne à partir de sa date de naissance (en années, mois, jours)
+            </p>
+        </div>
+        <hr>
+        <div>
+            <a href=" ./exo15.php" title="exo15">exo15 fait</a>
+            <p>
+            Créer une classe Personne (nom, prénom et date de naissance).
+            <br> 2 personnes et afficher leurs informations : nom, prénom et âge. 
+            <br>$p1 = new Personne("DUPONT", "Michel", "1980-02-19") ; 
+            <br>$p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17") ;           
+             </p>
+        </div>
+        <hr>
+ 
+
+    </main>
+
+</body>
+
+</html>
