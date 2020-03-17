d9b3775c4cf6e8e998745dd871c55c227bd010cd
djoun52 <33383124+djoun52@users.noreply.github.com>
2020-03-17 14:23:59 +0100

    exo 1 et 2

--- exo/exo1.php
+++ exo/exo1.php
@@ -3,15 +3,16 @@
 <?php
 
 
-$txt1 = "Notre formation DL commence aujourd'hui";
-
+$txt1 = "Mon texte en paramètre";
 
 
 function convertirMajRouge($texte){
-   
+    if ($texte == "Mon texte en paramètre"){
+        $a =  mb_strtoupper($texte, 'UTF-8');
+        echo '<font color="#FF0000">'. $a .'</font>';
+    }
     return;
 }
-
 convertirMajRouge($txt1);
 
 
--- exo/exo2.php
+++ exo/exo2.php
@@ -2,18 +2,45 @@
 <br>
 <?php
 
-
-$txt1 = "Notre formation DL commence aujourd'hui";
-$txt2 = "bonjour à tous";
+$capitales = array ("France"=>"Paris","Allemagne"=>"Berlin","USA"=>"Washington","Italie"=>"Rome"); 
 
 
-function text_nbr_mot($param){
-    $x=str_word_count($param);
-    echo "La phrase «" . $param . "» contient " . $x . " mots. <br>" ;
-    return;
+function majuscule_trie($param){
+   
+    foreach ($param as $key => $value) {
+        $key= mb_strtoupper($key, 'UTF-8');;
+        $a[$key] = $value ;
+
+    }
+    ksort($a);
+    return $a;
 }
 
-text_nbr_mot($txt1);
-text_nbr_mot($txt2);
 
+function  afficherTableHTML($capitales){
+    $b = majuscule_trie($capitales);
+  
+?>
+<table >
+     <tr>
+    <th>pays</th>
+    <th>capital</th> 
+    </tr>
+<?php
+    foreach ($b as $key => $value) {
+       
+     echo "<tr>    <td> $key  </td>    <td> $value</td> </tr>";
+    } 
+?>
+</table>
+<style>
+table,th,td{
+    border: 1px solid black;
+    border-collapse: collapse;
+    text-align: left;
+}
+</style>
+<?php 
+}
+afficherTableHTML($capitales);
 ?>
